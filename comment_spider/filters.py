a68dd4e6ad904af1721afe6ff8f9c372eeb0f5ca
Zack <2930777518@qq.com>
2023-10-31 10:47:08 +0800

    完善了filter，添加了settings文件

--- comment_spider/filters.py
+++ comment_spider/filters.py
@@ -5,6 +5,11 @@ from functools import partial
 
 class BaseFilter:
     def _clean(self, text):
+        """
+        文本清洗函数，如果需要自定义Filter需要继承BaseFilter，实现_clean方法，完成清洗。如果当前句子需要丢弃，则返回None
+        :param text: 需要清洗的文本
+        :return: 清洗后的文本，如果返回None表示丢弃该文本
+        """
         raise NotImplementedError()
 
     def clean(self, texts):
@@ -22,11 +27,27 @@ class BasicFilter(BaseFilter):
             exclude_sentences=None,
             allow_special_token=None
     ):
+        """
+        :param comment_regex: 文本内容需要匹配的模式
+        :param min_len: 最小长度，低于这个长度的文本会被过滤
+        :param max_len: 最大长度，高于这个长度的文本会被过滤
+        :param stop_words: 违禁词，list或文件路径，包含违禁词的句子会被过滤
+        :param exclude_sentences: 排除句，与排除句非常相似的句子（difflib的ratio >= 0.95）会被过滤
+        :param allow_special_token:
+        """
         self.comment_regex = comment_regex
         self.min_len = min_len
         self.max_len = max_len
-        self.stop_words = stop_words
-        self.exclude_sentences = exclude_sentences
+        if not isinstance(stop_words, list):
+            with open(stop_words, encoding='utf-8') as f:
+                self.stop_words = f.readlines()
+        else:
+            self.stop_words = stop_words
+        if not isinstance(exclude_sentences, list):
+            with open(exclude_sentences, encoding='utf-8') as f:
+                self.exclude_sentences = f.readlines()
+        else:
+            self.exclude_sentences = exclude_sentences
         self.allow_special_token = allow_special_token
 
     def _clean(self, comment):
---
+++ settings.py
@@ -0,0 +1,5 @@
+STOP_WORDS = './resources/stop_words'
+EXCLUDED_SENTENCES = './resources/excluded_sentences'
+COMMENTS_PERSIST = "./resources/sqlite"
+QA_PERSIST = "./resources/vector_store"
+SPEECHES = "./resources/speeches"
